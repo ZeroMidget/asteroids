e7441e7ce61512676756c9f069baa85d166dae47
ZeroMidget <zeromidget@gmail.com>
2025-07-25 16:23:02 -0400

    upload to github

---
+++ main.py
@@ -0,0 +1,52 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import sys
+from player import *
+from constants import *
+from asteroid import *
+from asteroidfield import *
+
+
+def main():
+    print ("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        updatable.update(dt)
+        
+        for asteroid in asteroids:
+            collision = asteroid.collide(player)
+            if collision == True:
+                print ("Game over!")
+                sys.exit()
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+
+
+        pygame.display.flip()
+        
+        dt = clock.tick(60)/1000
+
+if __name__ == "__main__":
+    main()
